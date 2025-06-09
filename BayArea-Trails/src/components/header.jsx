adb4700eb87e450241e296b9e6104fbf00ca17d3
Hector Baeza <SFhectorj@icloud.com>
2025-06-08 23:20:16 -0700

    Created header.jsx: Includes the title and short description.

---
+++ BayArea-Trails/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <Header className="app-header">
+            <h1>Bay Area Hikes & Trails</h1>
+            <p>Your community guide to the best hiking spots in the San Francisco Bay Area</p>
+        </Header>
+    );
+};
+
+export default Header;
